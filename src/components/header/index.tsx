73de638bf2adbed8413352fa28f4565b62fe5e4b
Abhishek <abhishek@tenxerlabs.com>
2024-02-08 01:31:27 +0530

    feat: added them colors

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,8 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import { Tenxerlab } from '@/components/logo';
 import { getTheme, toggleTheme } from '@/lib/features/theme/themeSlice'
+import AppBar from '@mui/material/AppBar';
+import Paper from '@mui/material/Paper';
 
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { Button } from '@mui/material'
@@ -11,15 +13,17 @@ const Header: React.FC = () => {
     const theme = useAppSelector(getTheme)
     const dispatch = useAppDispatch()
     return (
-        <header className="tw-h-full tw-border-b-[1px] tw-p-3 tw-shadow-md tw-flex tw-flex-row tw-justify-between">
-            <div className='tw-box-border tw-w-24'><Tenxerlab /></div>
-            <Button variant="contained" color="primary" >Contained</Button>
-            <div className=' tw-cursor-pointer' id="test-id-toggle-theme" onClick={() => dispatch(toggleTheme())}>
-                {
-                    theme === 'dark' ? <DarkModeIcon id="test-id-light-mode" />: <LightModeIcon id="test-id-dark-mode"/>
-                }
-            </div>
-        </header>
+        <Paper variant="outlined" square className='tw-h-full'>
+            <header className=" tw-h-full tw-flex tw-flex-row tw-justify-between ">
+                <div className='tw-box-border tw-w-24'><Tenxerlab /></div>
+                <Button variant="contained" color="primary" >Contained</Button>
+                <div className=' tw-cursor-pointer' id="test-id-toggle-theme" onClick={() => dispatch(toggleTheme())}>
+                    {
+                        theme === 'dark' ? <DarkModeIcon id="test-id-light-mode" /> : <LightModeIcon id="test-id-dark-mode" />
+                    }
+                </div>
+            </header>
+        </Paper>
     )
 }
 
--- src/lib/features/theme/index.tsx
+++ src/lib/features/theme/index.tsx
@@ -12,7 +12,11 @@ const lightTheme = createTheme({
     palette: {
         mode: 'light',
         background: {
-            default: '#F0F2F5', // Light background color
+            default: '#ececec',
+            paper: "#FFF"
+        },
+        primary: {
+            main: '#2f4ee4',
         },
     },
 });
@@ -21,7 +25,8 @@ const darkTheme = createTheme({
     palette: {
         mode: 'dark',
         background: {
-            default: '#212121', // Dark background color
+            default: '#2E2F32', // Dark background color
+            paper: "#0003"
         },
     },
 });
@@ -30,7 +35,7 @@ const darkTheme = createTheme({
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
 
     const themeType = useAppSelector(getTheme)
-    const theme: Theme = themeType === 'dark'? darkTheme: lightTheme
+    const theme: Theme = themeType === 'dark' ? darkTheme : lightTheme
     // console.log("create new theme")
 
     return <TM theme={theme}>{children}</TM>;
